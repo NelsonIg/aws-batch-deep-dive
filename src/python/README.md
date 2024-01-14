aa8d4885351c5918c949547cdff2046f880d09bb
n.igbokwe <nelson.igbokwe@web.de>
2024-01-14 10:30:41 +0100

    Build Docker image

--- src/python/README.md
+++ src/python/README.md
@@ -4,10 +4,48 @@
 + Python 3.11
 + AWS Access
 
-# Installation
+## Installation
 ```bash
 python -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 ```
+
+## Usage
+Export ENV Vars for defining S3 Parameters.
+
+```bash
+export BUCKET=<bucket-name>
+export PREFIX=<prefix>
+export OUTPUT_PREFIX=<output-prefix>
+```
+
+Run the script
+```bash
+python script.py
+```
+
+## BUILD DOCKER IMAGE
+ Build for local architecture.
+```bash
+docker build -t aws-batch-single . 
+```
+
+Build for ARM architecture.
+```bash
+docker buildx build --platform linux/arm64 -t aws-batch-single-arm --load .
+```
+
+Buil for AMD64 architecture.
+```bash
+docker buildx build --platform linux/amd64 -t aws-batch-single-amd64 --load .
+```
+
+## RUN DOCKER IMAGE
+Run Docker Image locally
+```bash
+docker run --rm  -e BUCKET=<bucket-name> -e PREFIX=<prefix> -e OUTPUT_PREFIX=<output-prefix> -e AWS_ACCESS_KEY_ID=<access-key> -e AWS_SECRET_ACCESS_KEY=<secret-access-key> -e AWS_SESSION_TOKEN=<session-token> -e AWS_DEFAULT_REGION=<region> aws-batch-single
+```
+
+
--- src/python/single/script.py
+++ src/python/single/script.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import json
 import boto3
+import os
 
+
+def format_prefix(prefix: str) -> str:
+    if prefix.endswith("/"):
+        return prefix
+    return f"{prefix}/"
 
 S3_RESOURCE = boto3.resource("s3")
-BUCKET = "test-nelson-igbokwe"
-PREFIX = "aws_batch_source/"
-OUTPUT_PREFIX = "aws_batch_output/"
+BUCKET = os.environ["BUCKET"]
+PREFIX = format_prefix(os.environ["PREFIX"])
+OUTPUT_PREFIX = format_prefix(os.environ["OUTPUT_PREFIX"])
 S3_BUCKET = S3_RESOURCE.Bucket(BUCKET)
 
 def main():
@@ -16,9 +22,19 @@ def main():
 def list_keys_s3(prefix: str) -> list:
     return [obj.key for obj in S3_BUCKET.objects.filter(Prefix=prefix) if obj.key.endswith(".json")]
 
+
+def process_keys(keys: list[str]) -> None:
+    for key in keys:
+        print(f"Processing {key}")
+        raw_data = read_json_s3(BUCKET, key)
+        transform_data_df = transform_data(raw_data)
+        save_data_s3(transform_data_df, key)
+
+
 def read_json_s3(bucket: str, key: str) -> dict:
     obj = S3_RESOURCE.Object(bucket, key)
     return json.loads(obj.get()["Body"].read().decode("utf-8"))
+
 
 def transform_data(raw_data: dict) -> pd.DataFrame:
     data_df = pd.DataFrame(raw_data["data"])
@@ -36,17 +52,14 @@ def transform_data(raw_data: dict) -> pd.DataFrame:
     data_df["timestamp"] = data_df["timestamp"].dt.tz_convert(None).dt.strftime("%Y-%m-%d")
     return data_df
 
+
 def save_data_s3(df: pd.DataFrame, key: str) -> None:
     filename = key.split("/")[-1].replace(".json", ".xlsx")
     output_path = f"s3://{BUCKET}/{OUTPUT_PREFIX}{filename}"
     df.to_excel(output_path, index=False)
 
 
-def process_keys(keys: list[str]) -> None:
-    for key in keys:
-        raw_data = read_json_s3(BUCKET, key)
-        transform_data_df = transform_data(raw_data)
-        save_data_s3(transform_data_df, key)
+
 
 if __name__ == "__main__":
     main()
